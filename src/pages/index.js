0bfc0ffdcfe1defc5cb0e0ce14f58b3073c3ebef
nemesdr238 <116638389+nemesdr238@users.noreply.github.com>
2023-11-29 23:54:00 +0100

    text delete

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,7 +118,7 @@ const links = [
     url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
 ]
